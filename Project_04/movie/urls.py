686d2347f49515aaa5a182ac9d8f545dd8cd8863
Kim Nok Hyeong <jessi0701@naver.com>
2019-02-08 08:25:50 +0000

    04프로젝트CRUD

---
+++ Project_04/movie/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from . import views
+
+
+
+urlpatterns = [
+    path("",views.list),
+    path("new/",views.new),
+    path("create/",views.create),
+    path("<int:id>/",views.read),
+    path("<int:id>/delete/",views.delete),
+    path("<int:id>/edit/",views.edit),
+    path("<int:id>/update/",views.update),
+    
+    ]
